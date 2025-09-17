d30e3b1963b1d7666e6a45141c2323a22228346f
250069-creator <250069@newuu.uz>
2025-09-17 11:01:03 +0300

    week 4

---
+++ practise for myself/main.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+ //     int abdulaziz;
+ //     double n;
+ //     cin >> n;
+ //     if ( n >4) {
+ //         cout << "Abdulaziz Gey" << endl;
+ //     } else {
+ //         cout << " abdulaziz not Gey" << endl;
+ //     }
+
+ // #include <iostream>
+ // using namespace std;
+ // int main() {
+ //     string name;
+ //     cout << "Enter your name :";
+ //     cin >> name;
+ //     if ( name == "Xudoyberdi") {
+ //         cout << " you are gey" << endl;
+ //     }
+ //     else {
+ //         cout << " you are the person in world" << endl;
+ //     }
+ //     return 0;
+ // }
+
+ // #include "iostream"
+ // using namespace std;
+ // int main() {
+ //
+ //     bool lightsOn = true;
+ //     return 0;
+ // }
+ // #include <iostream>
+ // using namespace std;
+ // int main() {
+ //     double score;
+ //     cout << " Students grading system " << endl;
+ //     cout << " Enter student's score " << endl;
+ //     cin >> score;
+ //     if (score > 85) {
+ //         cout << " You got A" ;
+ //     } else if ( score > 65) {
+ //         cout << " you got B ";
+ //     } else {
+ //         cout << " You got C";
+ //     }
+
+ //     const double PI = 3.14;
+ //     double radius, area;
+ //     cin >> radius;
+ //     if (radius >= 0) {
+ //         area = radius * radius * PI;
+ //         cout << " The area for the circle of " << " radius " << radius <<
+ //             " is : " << area << endl;
+
+ //     const double PI = 3.14;
+ //     double radius, area;
+ //     cin >> radius;
+ //
+ //    if (radius >= 0 ) {
+ //         area = PI * radius * radius;
+ //         cout << " The area for the circle of " << " radius " <<
+ //              radius << " is " << area << endl;
+ //     } else {
+ //         cout << " ERROR ";
+ //     }
+
+ //  int number;
+ //  cout << " Enter a integer number : ";
+ //  cin >> number;
+ //  if ( number % 5 == 0)
+ //   cout << " HiFive " << endl;
+ //  if ( number % 2 == 0)
+ //   cout << " HiEven " << endl;
+ //  return 0;
+ // }
+ //
+ // #include <iostream>
+ // using namespace std;
+ // int main () {
+ //  const double PI = 3.14;
+ //  double radius, area;
+ //  cout << " Enter the radius : " << endl;
+ //  cin >> radius;
+ //  if ( radius >= 0 ) {
+ //   area = PI * radius * radius;
+ //   cout << " The area of the circle is : " << area << endl;
+ //  } else {
+ //   cout << " the radius of the circle can't be less than zero" ;
+ //  }
+
+ // double score;
+ // cout << " Enter student score " ;
+ // cin >> score;
+ // if ( score >= 90) {
+ //  cout << " you got A" ;
+ // } else {
+ //  if ( score >= 70) {
+ //   cout << " You got B";
+ //  } else {
+ //   if ( score >= 60) {
+ //    cout << " You got C ";
+ //   } else {
+ //    cout << " You got F";
+ //   }
+ //  }
+ // }
+
+ // double score;
+ // cout << " Enter student score : ";
+ // cin >> score;
+ // if ( score >=90)
+ //  cout << " A ";
+ // else if ( score >= 80)
+ //  cout << " B ";
+ // else if ( score >= 70)
+ //  cout << " C ";
+ // else if ( score >= 60 )
+ //  cout << " D ";
+ // else cout << " F ";
+
+ // double score;
+ // cout << " Enter student score : ";
+ // cin >> score;
+ // if ( score >= 90)
+ //  cout << "A" ;
+ // else if ( score >= 80)
+ //  cout << " B " ;
+ // else if ( score >= 70)
+ //  cout << " C ";
+ // else if ( score >= 60 )
+ //  cout << " D ";
+ // else cout << " F ";
+
+// double x = 1.0 - 0.3 - 0.1 -0.1;
+//  if ( x == 0.5 )
+//   cout << " x is 0.5 " << endl;
+//  else
+//   cout << " x is not 0.5 " << endl;
+
+
+ return 0;
+}
+
+
+
